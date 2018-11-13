39c1429eabab9a06278aad3a77c4d70919df846e
hhiroki <h-hiroki@users.noreply.github.com>
2018-11-13 15:17:20 +0900

    ⚽️ ボタンのon, off機能

--- app/src/main/java/com/example/h_hiroki/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/h_hiroki/helloandroid/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.h_hiroki.helloandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) { // onCreateは画面起動時に最初に呼ばれるメソッド
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var onFlg = false
+
+        tapHere.setOnClickListener{
+
+            if (onFlg == false) {
+                textView.text = "turn on"
+                onFlg = true
+            } else {
+                textView.text = "turn off"
+                onFlg = false
+            }
+        }
     }
 }
